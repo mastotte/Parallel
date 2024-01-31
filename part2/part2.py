72264118bcef9d708a925d4005f94c06957a4bc9
mastotte <mastotte@ucsc.edu>
2024-01-30 18:28:41 -0800

    do it work

--- part2/part2.py
+++ part2/part2.py
@@ -23,11 +23,11 @@ def homework_reduction_source(partitions):
     function_body = ""
     function_body = f"\n  int partition_size = size / {partitions};"
     function_body += f"\n  for (int p = 0; p < {partitions}; ++p) {{"
-    function_body += f"\n    for (int i = p * partition_size + 1; i < (p + 1) * partition_size; ++i) {{"
+    function_body += f"\n    for (int i = (p * partition_size) + 1 ; i < (p + 1) * partition_size; ++i) {{"
     function_body += f"\n      a[p*partition_size] += a[i+(p*partition_size)];"
     function_body += "\n    }"
     function_body += "\n  }"
-    function_body += f"\n  for (int i = 0; i < {partitions}; ++i) {{"
+    function_body += f"\n  for (int i = 1; i < {partitions}; ++i) {{"
     function_body += "\n    a[0] += a[i * partition_size];"
     function_body += "\n}"
     # closing brace
